d4c1eb03383df3844b9ebe4d64e23d849d178429
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-07-06 14:06:31 +0200

    finished implementing vertical collision

--- main.py
+++ main.py
@@ -87,12 +87,21 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        # self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_velocity, self.y_velocity)
 
         self.fall_count += 1
         self.update_sprite()
         self.update()
+
+    def landed(self):
+        self.fall_count = 0
+        self.y_velocity = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self.count = 0
+        self.y_velocity *= -1
 
     def update_sprite(self):
         sprite_sheet = "idle"
@@ -158,7 +167,23 @@ def draw(window, background, bg_image, player, objects):
     pygame.display.update()
 
 
-def handle_move(player):
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+
+            collided_objects.append(obj)
+
+    return collided_objects
+
+
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_velocity = 0
@@ -166,6 +191,8 @@ def handle_move(player):
         player.move_left(PLAYER_VELOCITY)
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VELOCITY)
+
+    handle_vertical_collision(player, objects, player.y_velocity)
 
 
 def main(window):
@@ -188,7 +215,7 @@ def main(window):
                 break
 
         player.loop(FPS)
-        handle_move(player)
+        handle_move(player, floor)
         draw(window, background, bg_image, player, floor)
 
     pygame.quit()
